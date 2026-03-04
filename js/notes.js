a3972525b3d775fe6409e0bab81889582d95a145
Avinash <buildwithavinash@users.noreply.github.com>
2026-03-04 11:51:54 +0530

    added edit, delete, and cancel Edit feature in notes

--- js/notes.js
+++ js/notes.js
@@ -2,7 +2,15 @@ let formField = document.querySelector(".notes__form");
 const noteTitle = document.querySelector("#noteTitle");
 const noteDes = document.querySelector("#noteDes");
 let notesList = document.querySelector(".notes__list");
+let addNoteBtn = document.querySelector(".btn-addNotes");
+let cancelEditBtn = document.querySelector(".btn-cancelEdit");
+
+// states
 let notes = [];
+let inEditMode = false;
+let itemToEdit = null;
+
+
 
 formField.addEventListener("submit", (e)=>{
     e.preventDefault();
@@ -10,27 +18,41 @@ formField.addEventListener("submit", (e)=>{
     let noteHeading = noteTitle.value.trim();
     let noteDescription = noteDes.value.trim();
 
-    let newNote = {
+
+    if(inEditMode){
+        itemToEdit.noteHeading = noteHeading
+        itemToEdit.noteDes = noteDescription
+
+        inEditMode = false;
+        itemToEdit = null;
+        addNoteBtn.textContent = "Add Note";
+        cancelEditBtn.classList.add("hidden");
+    }else {
+         let newNote = {
         id: Date.now(),
         noteHeading : noteHeading,
         noteDes : noteDescription,
     }
 
     notes.push(newNote);
+    }
+    
     renderNotes(notes)
     console.log(notes);
 
+    formField.reset();
 })
 
 renderNotes(notes);
 
-function renderNotes(){
+function renderNotes(notes){
 
 notesList.innerHTML = "";
     notes.forEach((note)=>{
         
 const li = document.createElement('li');
 li.classList.add("note__item")
+li.dataset.id = note.id
 
 const leftDiv = document.createElement('div');
 leftDiv.classList.add("list");
@@ -51,7 +73,7 @@ rightDiv.classList.add("right");
 
 const noteDate = document.createElement('div');
 noteDate.classList.add("note__date")
-noteDate.textContent = '2nd march';
+noteDate.textContent = dateFormattter(note.id);
 
 const noteActions = document.createElement('div');
 noteActions.classList.add("note__actions");
@@ -71,4 +93,51 @@ li.append(leftDiv, rightDiv);
 
 notesList.append(li);
     })
+}
+
+notesList.addEventListener("click", function(e){
+    let target = e.target;
+    let card = target.closest(".note__item");
+    console.log(card);
+    if(!card) return;
+
+    if(target.classList.contains("btn-edit")){
+        inEditMode = true;
+        noteTitle.focus();
+
+        itemToEdit = notes.find((note)=> note.id === Number(card.dataset.id));
+        noteTitle.value = itemToEdit.noteHeading;
+        noteDes.value = itemToEdit.noteDes;
+        addNoteBtn.textContent = "Update"
+        cancelEditBtn.classList.remove("hidden");
+    }
+
+    if(target.classList.contains("btn-delete")){
+        inEditMode = false;
+        itemToEdit = null;
+        cancelEditBtn.classList.add("hidden");
+        addNoteBtn.textContent = "Add Note"
+        notes = notes.filter((note)=> note.id !== Number(card.dataset.id))
+        renderNotes(notes);
+    }
+})
+
+cancelEditBtn.addEventListener("click", function(e){
+    inEditMode = false;
+    itemToEdit = null;
+    formField.reset();
+    cancelEditBtn.classList.add("hidden");
+    addNoteBtn.textContent = "Add Note"
+})
+
+function dateFormattter(id){
+
+    const today = new Date(id);
+    const dateOptions = {
+        day : "numeric",
+        month : "long",
+        year : "numeric",
+    }
+    const formattedDate = today.toLocaleDateString("en-GB", dateOptions);
+    return formattedDate;
 }
